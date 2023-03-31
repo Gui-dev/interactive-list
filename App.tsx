66486f4c9f553d40a574e0c160818aa8cb79dbaa
Gui Silva <adrianon10@gmail.com>
2023-03-31 04:28:51 -0300

    feat: add component GestureHandlerRootView

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar'
 
 import { List } from './src/screens/List'
 
 export default function App () {
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar style="auto" backgroundColor="#222" translucent />
       <List />
-    </>
+    </GestureHandlerRootView>
   )
 }
